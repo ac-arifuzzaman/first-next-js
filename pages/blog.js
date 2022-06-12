cd28d8c613b066c489c3f4722e6aa290cf254cf8
arifuzzaman arif <ac.arifarifuzzaman1@gmail.com>
2022-06-12 22:59:04 +0600

    add manual steeps

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+// step 1: collect all the files from blogdata directorty
+// step 2: iterate through them and Display then
 
 const Blog = () => {
   return (
-    <div>
-      <h2>this is blogs page</h2>
+    <div className={styles.main}>
+      <div>
+        <Link href={"/blogpost/learn-javascript"}>
+          <h2 className={styles.blogitemh2}>How to learn JavaScript in 2022</h2>
+        </Link>
+        <p>JavaScript is a machine language</p>
+      </div>
+      <div>
+        <h2 className={styles.blogitemh2}>How to learn JavaScript in 2022</h2>
+        <p>JavaScript is a machine language</p>
+      </div>
+      <div>
+        <h2 className={styles.blogitemh2}>How to learn JavaScript in 2022</h2>
+        <p>JavaScript is a machine language</p>
+      </div>
+      <div>
+        <h2 className={styles.blogitemh2}>How to learn JavaScript in 2022</h2>
+        <p>JavaScript is a machine language</p>
+      </div>
     </div>
   );
 };
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,11 +1,35 @@
 import { useRouter } from "next/router";
 import React from "react";
+import styles from "../../styles/Home.module.css";
+
+// step 1: Find the file corresponding to the slug
+// step 2: Populate then inside the page
 
 const Post = () => {
   const router = useRouter();
   const { slug } = router.query;
 
-  return <p>{slug}</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+          voluptas, deleniti eos culpa, nihil aspernatur eveniet ullam nisi ipsa
+          officia excepturi et amet enim autem dignissimos explicabo. Aut iure
+          labore excepturi dolorum consequuntur ducimus dolore recusandae
+          aspernatur provident nobis minima, sit cupiditate inventore hic alias
+          distinctio nesciunt neque rerum expedita repudiandae accusantium fuga
+          eveniet enim ipsa! Sequi molestias rerum vitae nulla sapiente
+          reprehenderit tempora perspiciatis consequuntur delectus ut laborum
+          dolor possimus nostrum nam, voluptatem, distinctio accusamus, veniam
+          commodi ullam corrupti ipsa? Vel itaque atque porro rem voluptate
+          dolore?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
